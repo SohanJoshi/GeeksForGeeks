f40de49549814d27e5e026e31f27e395f56f34ce
Sohan <sohanjoshi1991@gmail.com>
2019-09-14 13:13:03 +0530

    First Commit from New Laptop

--- GeeksForGeeks/src/com/backtracking/hard/NQueenPuzzle.java
+++ GeeksForGeeks/src/com/backtracking/hard/NQueenPuzzle.java
@@ -5,13 +5,17 @@ import java.util.List;
 import java.util.Scanner;
 
 public class NQueenPuzzle {
-    boolean foundSolution = false;
+    private boolean foundSolution = false;
 
     public static void main (String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        new NQueenPuzzle().findNQueenSolutions(n);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            new NQueenPuzzle().findNQueenSolutions(n);
+        }
     }
 
     private void findNQueenSolutions(int boardSize) {
@@ -23,39 +27,56 @@ public class NQueenPuzzle {
             findNQueenSolutionsUtil(boardSize, i, 1, list, visited);
 
         if(!foundSolution)
-            System.out.println(-1);
+            System.out.print(-1);
+
+        System.out.println();
     }
 
     private void findNQueenSolutionsUtil(int boardSize, int currentColumn, int row, List<Integer> resultList, boolean[][] visited) {
-        if(row > boardSize)
+
+        if (!isSafe(boardSize, row, currentColumn, visited))
             return;
+
+        resultList.add(row - 1, currentColumn);
 
         if(row == boardSize)
             printResult(resultList);
 
-        for(int i = 1; i < row; i++)
+        else
         {
-            if(visited[i][currentColumn])
-                return;
-            if(currentColumn - row + i > 0 && visited[i][currentColumn - row + i])
-                return;
-            if(currentColumn + row - i <= boardSize && visited[i][currentColumn + row - i])
-                return;
+            visited[row][currentColumn] = true;
+
+            for(int i = 1; i <= boardSize; i ++)
+                findNQueenSolutionsUtil(boardSize, i, row + 1, resultList, visited);
+
+            visited[row][currentColumn] = false;
         }
 
-        visited[row][currentColumn] = true;
+        resultList.remove(row - 1);
 
-        resultList.add(row, currentColumn);
 
-        for(int i = 1; i <= boardSize; i ++)
-            findNQueenSolutionsUtil(boardSize, i, row + 1, resultList, visited);
-
-        visited[row][currentColumn] = false;
 
     }
 
-    private void printResult(List<Integer> resultList) {
-        System.out.println(resultList);
+    private boolean isSafe(int n, int r, int c, boolean[][] visited) {
+        for(int k = 1; k < r; k++)
+        {
+            if(visited[k][c])
+                return false;
+            if(c - r + k > 0 && visited[k][c - r + k])
+                return false;
+            if(c + r - k <= n && visited[k][c + r - k])
+                return false;
+        }
+        return true;
+    }
+
+    private void printResult(List<Integer> resultList)
+    {
+        foundSolution = true;
+        System.out.print("[");
+        resultList.forEach(i -> System.out.print(i + " "));
+        System.out.print("] ");
     }
 
 
